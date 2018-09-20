890f688d7b1361151cc5ce1a9ed3c0f295715c6e
David Winterbottom <david.winterbottom@gmail.com>
2018-09-20 16:51:06 +0100

    Fix issue with apt-get upgrade exiting with 100 code
    Likely cause: broken package dependencies. Note, you need to update the
    path for Exec targets to include /usr/local/bin and /usr/local/sbin for
    this to work.

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,7 +8,7 @@ class octo_base (
 
     # Upgrade all installed packages...
     exec { "update apt repositories":
-        command   => "time -p /usr/bin/apt-get update",
+        command   => "time -p /usr/bin/apt-get update --fix-missing",
         # Use a longer timeout as the default of 300 seconds fails more often than
         # we would like.
         timeout => 600,
@@ -16,7 +16,7 @@ class octo_base (
         logoutput => true,
     }
     exec { "upgrade installed packages":
-        command => "time -p /usr/bin/apt-get -y upgrade --fix-missing",
+        command => "time -p /usr/bin/apt-get -y upgrade --fix-missing --fix-broken",
         require => Exec["update apt repositories"],
         # Use a longer timeout as the default of 300 seconds fails more often than
         # we would like.
